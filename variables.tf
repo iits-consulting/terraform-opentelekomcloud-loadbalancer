e9aa789ea76e360f549a5acca40842c6ec807fb9
Can Aykin <can.aykin@iits-consulting.de>
2026-01-23 14:59:04 +0100

    - add count argument to be able to disable EIP for the loadbalancer - minor refactor to better align code structure with https://github.com/iits-consulting/terraform-opentelekomcloud-dedicated-loadbalancer

--- variables.tf
+++ variables.tf
@@ -3,16 +3,19 @@ variable "stage_name" {
   type        = string
   description = "Utilized to distinguish separate, but mostly equal environments within the same project. Usually dev, test, qa, prod."
 }
+
 variable "context_name" {
   type        = string
   description = "Short descriptive, readable label of the project you are working on. Is utilized as a part of resource names."
 }
+
 variable "bandwidth" {
   type        = number
-  default     = 300
-  description = "The bandwidth size. The value ranges from 1 to 1000 Mbit/s."
+  default     = 100
+  description = "The EIP bandwidth size. The value for a public loadbalancer ranges from 1 to 1000 Mbit/s. 0 disables the EIP, making the loadbalancer private."
 }
+
 variable "subnet_id" {
   type        = string
-  description = "Subnet where the elastic load balancer will be created."
+  description = "Subnet where the elastic load balancer will be created. "
 }
