5b1f1455be9d3636c17cf9bb5fe0342eea0ad482
Can Aykin <can.aykin@iits-consulting.de>
2026-08-20 14:36:40 +0200

    Expose full elb and eip resources as outputs, add descriptions to existing outputs

--- output.tf
+++ output.tf
@@ -1,11 +1,24 @@
 output "elb_id" {
-  value = opentelekomcloud_lb_loadbalancer_v2.elb.id
+  description = "ID of the loadbalancer."
+  value       = opentelekomcloud_lb_loadbalancer_v2.elb.id
 }
 
 output "elb_private_ip" {
-  value = opentelekomcloud_lb_loadbalancer_v2.elb.vip_address
+  description = "Private VIP address of the loadbalancer within its subnet."
+  value       = opentelekomcloud_lb_loadbalancer_v2.elb.vip_address
 }
 
 output "elb_public_ip" {
-  value = try(opentelekomcloud_vpc_eip_v1.ingress_eip[0].publicip[0].ip_address, null)
+  description = "Public address of the loadbalancer. Null when the EIP is disabled."
+  value       = var.bandwidth == 0 ? null : opentelekomcloud_vpc_eip_v1.ingress_eip[0].publicip[0].ip_address
+}
+
+output "elb" {
+  description = "The loadbalancer resource with all of its attributes."
+  value       = opentelekomcloud_lb_loadbalancer_v2.elb
+}
+
+output "eip" {
+  description = "The ingress EIP resource with all of its attributes. Null when the EIP is disabled."
+  value       = var.bandwidth == 0 ? null : opentelekomcloud_vpc_eip_v1.ingress_eip[0]
 }
